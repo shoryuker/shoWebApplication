eb1ed8950dfa4beb61981719f4fe5e8e57acec35
JonathanDTeague <blaketeague.jt@gmail.com>
2024-03-17 11:14:51 -0400

    linked css to the home template and things

--- mysite/Shoryuker/views.py
+++ mysite/Shoryuker/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def index(request):
-    return render(request, 'index.html')
+
+
+def home(request):
+    return render(request, 'home.html')
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
